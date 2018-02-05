e54cb19678e8732396459f257fcb3f6c191427a1
sigrung15 <sigrung15@ru.is>
2018-02-05 22:35:57 +0000

    I added a moving skybox instead of the cloads

--- Constrained Game/Assets/FarlandSkies/Demo/Scripts/Skybox/SkyboxRotator.cs
+++ Constrained Game/Assets/FarlandSkies/Demo/Scripts/Skybox/SkyboxRotator.cs
@@ -3,15 +3,9 @@
 public class SkyboxRotator : MonoBehaviour
 {
     public float RotationPerSecond = 1;
-    private bool _rotate;
 
     protected void Update()
     {
-        if (_rotate) RenderSettings.skybox.SetFloat("_Rotation", Time.time * RotationPerSecond);
-    }
-
-    public void ToggleSkyboxRotation()
-    {
-        _rotate = !_rotate;
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * RotationPerSecond);
     }
 }
