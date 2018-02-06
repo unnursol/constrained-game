416fc7cb846b9f53f79df456a21a31e5266823bf
sigrung15 <sigrung15@ru.is>
2018-02-06 18:54:02 +0000

    added a new package

---
+++ Constrained Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody2D rb2d;
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		/* if (Input.GetKeyDown ("a")) {
+			float x = rb2d.transform.position.x;
+			float y = rb2d.transform.position.y;
+
+			rb2d.MovePosition (new Vector2 (x + 0.2, y));
+		}*/
+	}
+}
---
+++ Constrained Game/Assets/Scripts/SkyboxRotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class SkyboxRotator : MonoBehaviour
+{
+    public float RotationPerSecond = 1;
+
+    protected void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * RotationPerSecond);
+    }
+}
