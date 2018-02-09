d4cca5980f6240f3d57a1c4eb1e3e7309b4a5b11
sigrung15 <sigrung15@ru.is>
2018-02-09 23:02:35 +0000

    added a start menu where you press any button to start the game, also added one pixel font for the start menu

---
+++ Constrained Game/Assets/Scripts/StartGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour {
+
+	private bool startGame;
+
+	public GameObject startScreen;
+
+	// Use this for initialization
+	void Start () {
+		Time.timeScale = 0;
+		startGame = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!startGame && Input.anyKey) {
+			startScreen.SetActive (false);
+			Time.timeScale = 1;
+		}
+	}
+}
