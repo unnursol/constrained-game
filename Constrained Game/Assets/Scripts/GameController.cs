e2cd053ee2be0e79ec398a3a0fda332e1d274daf
Unnur Sól Ingimarsdóttir <unnuri15@ru.is>
2018-02-12 22:11:17 +0000

    Win state

--- Constrained Game/Assets/Scripts/GameController.cs
+++ Constrained Game/Assets/Scripts/GameController.cs
@@ -12,10 +12,12 @@ public class GameController : MonoBehaviour
     private bool endGame;
     private bool winGame;
 
-    //private Time time;
+    [Header("Timer")]
     public Text timerText;
     public float startTime;
 
+
+    [Header("Player Variables")]
     public int player1score;
     public int player2score;
     public Text p1ScoreText;
@@ -23,6 +25,11 @@ public class GameController : MonoBehaviour
     public Transform p1Bag;
     public Transform p2Bag;
     public GameObject smallPotato;
+
+
+    [Header("Win Screen Variables")]
+    public Text winner;
+    public Text message;
 
 
     public GameObject startScreen;
@@ -88,6 +95,7 @@ public class GameController : MonoBehaviour
         {
             UnityEngine.Debug.Log(t);
             Time.timeScale = 0;
+            UpdateWinner();
 
             winGame = true;
             winScreen.SetActive(true);
@@ -112,5 +120,33 @@ public class GameController : MonoBehaviour
 
             timerText.text = minutes + ":" + seconds;
         }
+    }
+
+    private void UpdateWinner()
+    {
+        if(player2score == player1score)
+        {
+            winner.text = "IT'S A DRAW!";
+            message.text = "Neither of you managed to gain the upper hand and are therefore forced to compete in price, " +
+                "constantly lowering to gain the upper hand. Because of this you couldn't afford seeds " +
+                "for next years crops and both went out of business. Try again?";
+        }
+        else
+        {
+            string looser = "";
+            if (player2score > player1score)
+            {
+                winner.text = "WINNER IS: PLAYER 1!";
+                looser = "player 2";
+            }
+            else if (player2score < player1score)
+            {
+                winner.text = "WINNER IS: PLAYER 2!";
+                looser = "Player 1";
+            }
+            message.text = "You have the upper hand and control the market price of potatoes, lowering it until " + looser + " goes out of business. " +
+                           "You now have potato monopoly and make millions! " +
+                           "" + looser + "'s asks you for a job which you happily give him....as a butler.";
+        }     
     }
 }
