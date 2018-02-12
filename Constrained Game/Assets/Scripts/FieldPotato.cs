8abd31a82b54d5c9b359935eb8e1c31f164cd143
Birkir Brynjarsson <birkir@clients-pool-6.nat.ru.is>
2018-02-12 10:29:03 +0000

    Potato Farm

--- Constrained Game/Assets/Scripts/FieldPotato.cs
+++ Constrained Game/Assets/Scripts/FieldPotato.cs
@@ -9,12 +9,24 @@ public class FieldPotato : MonoBehaviour {
 	public SpriteRenderer spriteRenderer;
 	public int level;
 	private int maxLevel;
+	public float growUpdateTime;
+	private float lastUpdate;
 
 	void Start(){
 		maxLevel = potatoes.Length;
+		lastUpdate = Time.time;
+	}
+
+	void Update(){
+		Debug.Log (lastUpdate);
+		Debug.Log (Time.time);
+		if (lastUpdate <= Time.time - growUpdateTime) {
+			levelUp ();
+		}
 	}
 
 	public void levelUp(){
+		lastUpdate = Time.time;
 		level = level < maxLevel ? level + 1 : level;
 		if (level != 0) {
 			spriteRenderer.sprite = potatoes [level - 1];
@@ -22,11 +34,18 @@ public class FieldPotato : MonoBehaviour {
 	}
 
 	public void levelDown(){
+		lastUpdate = Time.time;
 		level = level == 0 ? 0 : level - 1;
 		if (level == 0) {
 			spriteRenderer.sprite = null;
 		} else {
 			spriteRenderer.sprite = potatoes [level - 1];
+		}
+	}
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "ammunation") {
+			Destroy (other.gameObject);
+			levelDown ();
 		}
 	}
 }
